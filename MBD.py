27be4c247678975ac7542f365ec3e1efec0b8219
Olzheke1407 <144256804+Olzheke1407@users.noreply.github.com>
2023-09-17 20:43:10 +0500

    MBD.py

--- MBD.py
+++ MBD.py
@@ -1,15 +1,15 @@
 import os
 
 def split_file_path(file_path):
-    # Разделить путь на путь и имя файла
+    
     directory, filename = os.path.split(file_path)
 
-    # Разделить имя файла на имя и расширение
+    
     name, extension = os.path.splitext(filename)
 
     return directory, name, extension
 
-# Пример использования:
+
 file_path = r"\Users\Public\BlueStacks.txt"
 path, name, extension = split_file_path(file_path)
 print("Путь:", path)
